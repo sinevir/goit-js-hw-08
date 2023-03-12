bc85bea6d78ca7a5eb99cf6e035dbf3d3db35690
Oleh Stashchyshyn <o.s.dubbie@gmail.com>
2023-03-12 23:35:26 +0200

    Update 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,20 @@ import { throttle } from 'lodash';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+console.log(localStorage)
+
 player.on('timeupdate',  throttle( event => {
     localStorage.setItem('videoplayer-current-time', event.seconds);
     }, 1000)
 );
+console.log('localStorage', localStorage.length)
 
-player
+if(localStorage.length === 0) {
+    return
+} else {
+    player
 .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
 .catch(function (error) {
     console.error(error)
 });
+}
